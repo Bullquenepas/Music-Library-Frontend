c7c58818d67cd4768c01a02d46e71c1f0fac3012
Christopher Bonilla <Bullquenepas>
2022-04-29 08:31:49 -0400

    Added axios package + functionality to App.js + component files.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import React, { useState } from 'react';
 
 function App() {
 
-const [songs, setSongs] = useState([])
+const [songs, setSongs] = useState([]);
 
+useEffect(() => {
+  getAllSongs();
+}, [])
+
+async function getAllSongs(){
+  let response = await axios.get("http://127.0.0.1:8000/music/");
+  setSongs(response.data);
+}
+
+async function createSong(newSong){
+  let response = await axios.post("http://127.0.0.1:8000/music/", newSong);
+  if(response.status === 201){
+    await getAllSongs();
+  }
+}
 
   return (
     <div>
